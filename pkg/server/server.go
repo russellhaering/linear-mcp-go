25ac12b6f472c0dc1319d5eb915f893680f8c9b4
Gero Posmyk-Leinemann <gero@gitpod.io>
2025-10-08 15:00:12 +0000

    Bump version to 1.15.0
    Co-authored-by: Ona <no-reply@ona.com>

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -14,7 +14,7 @@ const (
 	// ServerName is the name of the MCP server
 	ServerName = "Linear MCP Server"
 	// ServerVersion is the version of the MCP server
-	ServerVersion = "1.14.0"
+	ServerVersion = "1.15.0"
 )
 
 // LinearMCPServer represents the Linear MCP server
